7cfc6dda131b01879796a76c5f37d0ace632b10a
Sixing <sx.ma223@gmail.com>
2023-07-25 20:26:17 +0800

    Update and rename main-new4.js to main-new5.js

--- main-new5.js
+++ main-new5.js
@@ -182,7 +182,7 @@
             }
             q = "TE_20220815_00"
         }
-        var F = "https://cdn.jsdelivr.net/gh/Sixing/extension@main/translator-new4.js";
+        var F = "https://cdn.jsdelivr.net/gh/Sixing/extension@main/translator-new5.js";
         if (p._cjlc)
             p._cjlc(F);
         else {
